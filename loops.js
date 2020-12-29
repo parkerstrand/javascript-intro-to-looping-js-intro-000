f9be0fee939bfff5f0dfb90333811b384f8b2522
Parker Strand <parker.strand@verizon.net>
2020-12-29 19:11:15 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -23,8 +23,8 @@ function doWhileLoop(array) {
     return Math.random() >= 0.5
   }
 
-  do {
-    pop();
-  } while (array.length > -1 && maybeTrue());
+  do 
+    array.pop();
+  while (array.length > -1 && maybeTrue());
     return array;
 }
